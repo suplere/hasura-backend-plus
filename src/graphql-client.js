2f90728d62fefe449b2b70b78c537687d2c032bc
Evzen Supler <evzen.supler@gmail.com>
2020-02-04 15:54:35 +0100

    First commit - based on nhost/hasura-backend-plus

---
+++ src/graphql-client.js
@@ -0,0 +1,9 @@
+const { GraphQLClient } = require('graphql-request');
+const { HASURA_GRAPHQL_ENDPOINT, HASURA_GRAPHQL_ADMIN_SECRET } = require('./config');
+
+exports.graphql_client = new GraphQLClient(HASURA_GRAPHQL_ENDPOINT, {
+    headers: {
+        'Content-Type': 'application/json',
+        'x-hasura-admin-secret': HASURA_GRAPHQL_ADMIN_SECRET,
+    },
+});
